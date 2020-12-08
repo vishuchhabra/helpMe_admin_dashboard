ad07f834651c2f476321ace717136d175b43ef2f
vishuchhabra <vishuchhabra1016@gmail.com>
2020-12-08 13:11:51 +0530

    adding deployment

--- projfrontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Routes from "./Routes";
-import ReactDOM from "react-dom";
-import {Router,Route,Link,HashRouter} from 'react-router-dom'
-
-
-ReactDOM.render( <Routes /> , document.getElementById("root"));
